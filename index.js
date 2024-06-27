4548d817c2aabf65f9f584c9ffd34112fb6b0810
BigBlueHat <byoung@bigbluehat.com>
2024-06-27 15:30:50 -0400

    Detect media/mime types for files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const dataurl = require('dataurl');
+const mime = require('mime-types')
 
 const pathToSomeImage = process.argv[2];
 
@@ -8,7 +9,8 @@ if (!pathToSomeImage) {
   console.error('Filename is required.');
 } else {
   const image = fs.readFileSync(pathToSomeImage);
+  const mimetype = mime.lookup(pathToSomeImage);
   // TODO: detect media type
-  const url = dataurl.convert({ data: image, mimetype: 'image/png' });
-  console.dir(url);
+  const url = dataurl.convert({ data: image, mimetype });
+  process.stdout.write(url);
 }
